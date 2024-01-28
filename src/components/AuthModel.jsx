6899fa6094aeef4d43287f544cc8a8442967d3ee
pi2411 <ygalna9764@gmail.com>
2024-01-28 15:26:52 +0200

    server

--- src/components/AuthModel.jsx
+++ src/components/AuthModel.jsx
@@ -1,62 +1,85 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
+import { useCookies } from "react-cookie";
 
-const AuthModel = ({setShowModel , isSingUp}) => {
-  const[email,setEmail]=useState(null);
-  const[password,setPassword]=useState(null);
-  const[confirmPasword,setConfirmPassword]=useState(null);
-  const[error,setError]=useState(null);
+const AuthModel = ({ setShowModel, isSingUp }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPasword, setConfirmPassword] = useState(null);
+  // const [error, setError] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies('null');
+  ;
 
-  console.log(email,password,confirmPasword)
-    function heandleClick(){
-        setShowModel(false)
+  let navigate = useNavigate()
 
-    }function heandleSubmit(e){
-    e.preventefault()
+
+
+  console.log(email, password, confirmPasword)
+  function heandleClick() {
+    setShowModel(false)
+  }
+
+  async function heandleSubmit(e) {
+    e.preventDefault();
     try {
-      if(isSingUp && (password !== confirmPasword)){
-        console.log("Password need to match!")
-        }
-      console.log("Mack post request to our database")
-    }catch(error){
-      console.log(error)
+      if (isSingUp && password !== confirmPasword) {
+        console.log("Passwords need to match!");
+        return;
+      }
+
+      console.log("posting", email, password);
+      const response = await axios.post(`http://localhost:8000/${isSingUp ? 'signup' : 'login'}`, { email, password })
+
+      setCookie('AuthToken', response.data.token)
+      setCookie('UserId', response.data.userId)
+
+      const success = response.status === 201;
+
+      if (success && isSingUp) navigate('/onboarding');
+      if (success && !isSingUp) navigate('/dashboard');
+      console.log("Made post request to our database");
+    } catch (error) {
+      console.log(error);
     }
- }
+  }
+
   return (
     <div className='auth-modal'>
-    <div className='close-icon' onClick={heandleClick}><IoMdCloseCircleOutline/></div>
-    <h2>{isSingUp? "NTCREATE ACCOU" : "Sing In"}</h2>
-    <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
-    <form onSubmit={heandleSubmit}>
-    <input 
-      type='email'
-      id="email"
-      name='email'
-      placeholder='email'
-      required={true}
-      onChange={(e)=>setEmail(e.target.value)}
-    />
-    <input 
-      type='password'
-      id="password"
-      name='password'
-      placeholder='password'
-      required={true}
-      onChange={(e)=>setPassword(e.target.value)}
-    />
-   {isSingUp && <input 
-      type='password'
-      id="password-check"
-      name='password-check'
-      placeholder='confirm-password'
-      required={true}
-      onChange={(e)=>setConfirmPassword(e.target.value)}
-    />}
-    <input className='secondary-button' type='submit' />
-    <p>{error}</p>
-    </form>
-    <hr/>
-    <h2>GET THE APP</h2>
+      <div className='close-icon' onClick={heandleClick}><IoMdCloseCircleOutline /></div>
+      <h2>{isSingUp ? "NTCREATE ACCOU" : "Sing In"}</h2>
+      <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+      <form onSubmit={heandleSubmit}>
+        <input
+          type='email'
+          id="email"
+          name='email'
+          placeholder='email'
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          id="password"
+          name='password'
+          placeholder='password'
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSingUp && <input
+          type='password'
+          id="password-check"
+          name='password-check'
+          placeholder='confirm-password'
+          required={true}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />}
+        <input className='secondary-button' type='submit' />
+        {/* <p>{error}</p> */}
+      </form>
+      <hr />
+      <h2>GET THE APP</h2>
 
     </div>
   )
